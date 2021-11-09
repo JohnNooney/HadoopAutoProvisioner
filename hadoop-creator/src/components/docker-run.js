c26ab2c99b04e2307168d81e819884c7da393922
John Nooney <1803534@uad.ac.uk>
2021-11-09 15:38:57 +0000

    Add POST button functionality
    send test data to API server

--- hadoop-creator/src/components/docker-run.js
+++ hadoop-creator/src/components/docker-run.js
@@ -3,21 +3,40 @@ import {Button} from 'antd';
 function DockerRun() {
   return (
     <div className="DockerRun">
-          <Button type="primary" onClick={buttonClick}>Button</Button>
+          <Button type="primary" onClick={buttonClickGet}>GET</Button>
+          <Button type="primary" onClick={buttonClickPost}>POST</Button>
     </div>
   );
 }
 
-function buttonClick(){
-    
+function buttonClickGet(){
+
     console.log("button click");
     fetch('http://localhost:5000/build', {
         method:'GET',
          headers: {
-            'Content-Type': 'application/json',
-            'Host':'http://localhost:5000',
-            'Origin':'http://localhost:3000'
+            'Content-Type': 'application/json'
         }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function buttonClickPost(){
+    const data = {"data":"test from react"}
+
+    console.log("button click");
+    fetch('http://localhost:5000/build', {
+        method:'POST',
+         headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(data)
     })
     .then(response => response.json())
     .then(data => {

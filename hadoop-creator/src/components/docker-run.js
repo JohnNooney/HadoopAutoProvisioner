618fd729466cc07fab7a02a8d85ce4a5b3ef919d
John Nooney <1803534@uad.ac.uk>
2021-11-09 16:33:24 +0000

    Add user notifications for status updates

--- hadoop-creator/src/components/docker-run.js
+++ hadoop-creator/src/components/docker-run.js
@@ -1,4 +1,4 @@
-import {Button} from 'antd';
+import {Button, notification} from 'antd';
 
 function DockerRun() {
   return (
@@ -11,7 +11,13 @@ function DockerRun() {
 
 function buttonClickGet(){
 
-    console.log("button click");
+    // GET start notification
+    const notif = {
+        "type":"info", 
+        "title":"GET Status", 
+        "desc":"The GET request has been sent. Please wait for a response."};
+    openNotification(notif);
+
     fetch('http://localhost:5000/build', {
         method:'GET',
          headers: {
@@ -21,16 +27,36 @@ function buttonClickGet(){
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
+
+        // success message to user
+        const notif = {
+            "type":"success", 
+            "title":"GET Status", 
+            "desc":"The GET request has been received: \n " + JSON.stringify(data)};
+        openNotification(notif);
     })
     .catch((error) => {
         console.error('Error:', error);
+
+        // error message to user
+        const notif = {
+            "type":"error", 
+            "title":"GET Status", 
+            "desc":"The GET request has failed: " + error};
+        openNotification(notif);
     });
 }
 
 function buttonClickPost(){
     const data = {"data":"test from react"}
 
-    console.log("button click");
+     // POST start notification
+     const notif = {
+        "type":"info", 
+        "title":"POST Status", 
+        "desc":"The POST request has been sent. Please wait for your container to start..."};
+    openNotification(notif);
+
     fetch('http://localhost:5000/build', {
         method:'POST',
          headers: {
@@ -41,10 +67,29 @@ function buttonClickPost(){
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
+        // success message to user
+        const notif = {
+            "type":"success", 
+            "title":"POST Status", 
+            "desc":"The POST request has been received. Your Container ID: " + JSON.stringify(data["containerId"])};
+        openNotification(notif);
     })
     .catch((error) => {
         console.error('Error:', error);
+        // error message to user
+        const notif = {
+            "type":"error", 
+            "title":"POST Status", 
+            "desc":"The POST request has failed: " + error};
+        openNotification(notif);
     });
+}
+
+function openNotification(details){
+    notification[details["type"]]({
+        message: details["title"],
+        description: details["desc"],
+      });
 }
 
 export default DockerRun;

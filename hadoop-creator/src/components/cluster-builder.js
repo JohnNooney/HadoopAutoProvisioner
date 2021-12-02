329a0132bb5a1c6bf2fc08270f47a2d5f7559884
John Nooney <1803534@uad.ac.uk>
2021-12-02 14:03:50 +0000

    Update for use with new component

--- hadoop-creator/src/components/cluster-builder.js
+++ hadoop-creator/src/components/cluster-builder.js
@@ -1,18 +1,45 @@
-import {Row, Col } from 'antd';
-import DockerRun from './docker-run';
+import {Row, Col, Select  } from 'antd';
+import ButtonRequest from './button-request';
+import { useState } from 'react';
+
+const { Option } = Select;
 
 function ClusterBuilder() {
   return (
     <div className="ClusterBuilder">
-      <Row justify="start" align="bottom">
-        <Col>
-          <h1>Press the button below to spin-up a cluster</h1>
+      <Row justify="end" align="middle">
+        < Col >
+          <ButtonRequest
+            requestType = "GET"
+            buttonText="Test API"
+            buttonColor="primary"
+            notificationCustomMsg = "Please wait for a response..."
+            displayPayload = "true"
+            payloadCustomMsg = "Your response: "
+          />
         </Col>
       </Row>
+      <Row justify="start" align="bottom">
+        <Col>
+          <h1>Press the button below to spin-up a cluster or test container</h1>
+        </Col>
+        
+      </Row>
+      
 
       <Row justify="start" align="middle">
         <Col>
           <DockerRun/>
+        <Col>
+          <ButtonRequest
+            requestType = "POST"
+            buttonText="Spin-Up"
+            buttonColor="primary"
+            notificationCustomMsg = "Please wait for your container to start..."
+            displayPayload = "true"
+            payloadCustomMsg = "Your Container ID: "
+            postData = {{"data":activeSelector}}
+          />
         </Col>
       </Row>
     </div>

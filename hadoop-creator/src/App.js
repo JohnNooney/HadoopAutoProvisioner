4e75ee7bec5de80a79c7553fe281b875b0c90e03
John Nooney <1803534@uad.ac.uk>
2022-02-22 11:38:25 +0000

    Refactor rename app

--- hadoop-creator/src/App.js
+++ hadoop-creator/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hadoop Cluster Builder
+          Auto Hadoop Provisioner
         </p>
       </header>
 

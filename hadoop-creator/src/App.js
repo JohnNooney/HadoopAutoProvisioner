164c84bd57d77f18989095b09b612dbcbfa502f6
John Nooney <1803534@uad.ac.uk>
2022-02-28 16:34:15 +0000

    Add nav bar

--- hadoop-creator/src/App.js
+++ hadoop-creator/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { useState } from 'react';
+import { Menu } from 'antd';
+import { ClusterOutlined, DashboardOutlined, SettingOutlined } from '@ant-design/icons';
 
 import ClusterBuilder from './components/cluster-builder';
 
 function App() {
+  const [currentNav, setCurrentNav] = useState({current:"builder"});
+
+  function handleClick(e) {
+    console.log('click ', e);
+    setCurrentNav({ current: e.key });
+    console.log('current Nav: ', currentNav);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +21,22 @@ function App() {
           Auto Hadoop Provisioner
         </p>
       </header>
+
+      <Menu 
+        onClick={handleClick} 
+        selectedKeys={[currentNav]}
+        mode="horizontal"
+      >
+        <Menu.Item key="builder" icon={<SettingOutlined />}>
+          Cluster Builder
+        </Menu.Item>
+        <Menu.Item key="diagram" icon={<ClusterOutlined />}>
+          Cluster Diagram
+        </Menu.Item>
+        <Menu.Item key="monitor" icon={<DashboardOutlined />}>
+          Cluster Monitoring
+        </Menu.Item>
+      </Menu>
 
       <div>
         <ClusterBuilder/>

f2b40d317290390a385fd9372d3fd8b8bd2051d6
John Nooney <1803534@uad.ac.uk>
2022-03-01 17:12:06 +0000

    Add template for scraper

--- flask-api/endpoints/scraper.py
+++ flask-api/endpoints/scraper.py
@@ -7,6 +7,11 @@ class Scraper(Resource):
         self.origin = 'http://localhost:3000' # only allow request from this host
         self.status = 200
         self.payload = "default"
+        self.namenode = "http://localhost:50070"
+        self.yarn = "http://localhost:8088"
+        self.datanodes = ["http://localhost:8042/", "http://localhost:8043/"] # TODO: set dynamically
+        self.spark = "http://localhost:"
+        self.sparknotebook = "http://localhost:"
 
     def get(self):
         parser = reqparse.RequestParser()
@@ -17,7 +22,14 @@ class Scraper(Resource):
         print("GET incoming data: ", args['data'])
 
         self.status = 200
-        self.payload = {"payload": "test"}
+
+        # TODO: payload should be set with data from getClusterDetails()
+        self.payload = {"payload": {
+            "namenode":self.namenode,
+            "yarn":self.yarn,
+            "datanodes":self.datanodes,
+            "spark":self.spark}}
+
         return self.payload, \
                self.status, \
                {'Access-Control-Allow-Origin': self.origin}  # return data and 200 OK code
@@ -30,3 +42,11 @@ class Scraper(Resource):
                 'Access-Control-Allow-Methods': 'GET, OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type',
                 'Content-Type': 'application/json'}
+
+    # if ran in container / stopping will also need to be in container
+    def getClusterDetails(self):
+        # docker - compose - f ./hadoop-cluster/docker-compose.yml up - d
+        # result = subprocess.check_output(['docker', 'compose', '-f', 'hadoop-cluster/base-docker-compose.yml', 'up', '-d'])
+        # print("subprocess response: " + result.decode())
+        # return result.decode()
+        return "test"

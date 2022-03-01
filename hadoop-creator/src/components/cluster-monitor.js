75f4a3ad15b223c448dd9e5c326007e002fa967f
John Nooney <1803534@uad.ac.uk>
2022-03-01 13:18:43 +0000

    Add simple dashboard

--- hadoop-creator/src/components/cluster-monitor.js
+++ hadoop-creator/src/components/cluster-monitor.js
@@ -1,4 +1,4 @@
-import {Row, Col, Select, Divider, Switch, Form, Button, Input, Space, Empty  } from 'antd';
+import {Row, Col, Select, Divider, Card, Space, Empty  } from 'antd';
 import ButtonRequest from './button-request';
 import { useState } from 'react';
 
@@ -6,6 +6,12 @@ const { Option } = Select;
 
 function ClusterMonitor(props) {
   // const [activeSelector, setActiveSelector] = useState("");
+  const [clusterDetails, setClusterDetails] = useState();
+  const defaultData = {
+    namenode : "http://localhost:50070/",
+    yarn : "http://localhost:8088/",
+    spark : ""
+  };
   
   return (
     <div className="cluster_monitor">
@@ -16,6 +22,32 @@ function ClusterMonitor(props) {
           </Col>
         </Row>
         <Divider/>
+      </div>
+
+      <div className='cluster_monitor_dashboard'> 
+        <Row justify="center" align="top" gutter={[40,24]}>
+            <Col align="start">
+              <Card title="Name Node Details">
+              <p><a href={defaultData.namenode}>Name Node WebUI</a></p>
+              </Card>
+            </Col>
+            <Col align="start">
+              <Card title="Data Node Details">
+              <p><a href={defaultData.datanode1}>Data Node 1 Manager</a></p>
+              <p><a href={defaultData.datanode2}>Data Node 2 Manager</a></p>
+              </Card>
+            </Col>
+            <Col align="start">
+              <Card title="Resource Manager Details">
+              <p><a href={defaultData.yarn}>YARN Web UI</a></p>
+              </Card>
+            </Col>
+            <Col align="start">
+              <Card title="Spark Details">
+              <p><a href={defaultData.spark}>Spark UI</a></p>
+              </Card>
+            </Col>
+        </Row>
       </div>
 
       {props.clusterData == null ? <Empty description={<span>No cluster built yet...</span>}/> : null} 

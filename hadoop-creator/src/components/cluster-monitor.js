40f84a4baa94db857df9c1db1ebd519b2a86e591
John Nooney <1803534@uad.ac.uk>
2022-03-01 11:59:00 +0000

    Add use of cluster state data

--- hadoop-creator/src/components/cluster-monitor.js
+++ hadoop-creator/src/components/cluster-monitor.js
@@ -1,21 +1,24 @@
-import {Row, Col, Select, Divider, Switch, Form, Button, Input, Space  } from 'antd';
+import {Row, Col, Select, Divider, Switch, Form, Button, Input, Space, Empty  } from 'antd';
 import ButtonRequest from './button-request';
 import { useState } from 'react';
 
 const { Option } = Select;
 
-function ClusterMonitor() {
+function ClusterMonitor(props) {
   // const [activeSelector, setActiveSelector] = useState("");
   
   return (
     <div className="cluster_monitor">
-      <div className='cluster_monitor'> 
+      <div className='cluster_monitor_title'> 
         <Row justify="center" align="bottom">
           <Col>
-            <h1>Explore your cluster.</h1>
+            <h1>Monitor your cluster.</h1>
           </Col>
         </Row>
-      </div> 
+        <Divider/>
+      </div>
+
+      {props.clusterData == null ? <Empty description={<span>No cluster built yet...</span>}/> : null} 
     </div>
   );
 }

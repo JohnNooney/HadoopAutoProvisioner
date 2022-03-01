92fbb419466bfd8f6403e3cd7d5be54833cf76ca
John Nooney <1803534@uad.ac.uk>
2022-03-01 14:55:53 +0000

    Update to use a custom dashboard component

--- hadoop-creator/src/components/cluster-monitor.js
+++ hadoop-creator/src/components/cluster-monitor.js
@@ -1,6 +1,7 @@
 import {Row, Col, Select, Divider, Card, Space, Empty  } from 'antd';
 import ButtonRequest from './button-request';
 import { useState } from 'react';
+import ClusterDashboard from './monitor-components/dashboard';
 
 const { Option } = Select;
 
@@ -10,7 +11,8 @@ function ClusterMonitor(props) {
   const defaultData = {
     namenode : "http://localhost:50070/",
     yarn : "http://localhost:8088/",
-    spark : ""
+    spark : "",
+    datanodes : ["http://localhost:8042/","http://localhost:8043/"]
   };
   
   return (
@@ -24,33 +26,7 @@ function ClusterMonitor(props) {
         <Divider/>
       </div>
 
-      <div className='cluster_monitor_dashboard'> 
-        <Row justify="center" align="top" gutter={[40,24]}>
-            <Col align="start">
-              <Card title="Name Node Details">
-              <p><a href={defaultData.namenode}>Name Node WebUI</a></p>
-              </Card>
-            </Col>
-            <Col align="start">
-              <Card title="Data Node Details">
-              <p><a href={defaultData.datanode1}>Data Node 1 Manager</a></p>
-              <p><a href={defaultData.datanode2}>Data Node 2 Manager</a></p>
-              </Card>
-            </Col>
-            <Col align="start">
-              <Card title="Resource Manager Details">
-              <p><a href={defaultData.yarn}>YARN Web UI</a></p>
-              </Card>
-            </Col>
-            <Col align="start">
-              <Card title="Spark Details">
-              <p><a href={defaultData.spark}>Spark UI</a></p>
-              </Card>
-            </Col>
-        </Row>
-      </div>
-
-      {props.clusterData == null ? <Empty description={<span>No cluster built yet...</span>}/> : null} 
+      {props.clusterData == null ? <Empty description={<span>No cluster running...</span>}/> : <ClusterDashboard Data={defaultData}/>} 
     </div>
   );
 }
---
+++ hadoop-creator/src/components/monitor-components/dashboard.js
@@ -0,0 +1,53 @@
+import {Row, Col, Select, Divider, Space, Card } from 'antd';
+
+
+const { Option } = Select;
+
+function ClusterDashboard(props) {
+
+    let counter = 0;
+
+    function listDataNodes()
+    {
+        let counter = 1;
+        let elems = "";
+        props.Data.datanodes.forEach(element => {
+            elems = elems+ "<p><a href="+element+">Data Node "+counter+" Manager</a></p>";
+            counter++;
+        });
+
+        return elems
+    }
+    
+    return (
+        <div className='cluster_monitor_dashboard'> 
+            <Row justify="center" align="top" gutter={[40,24]}>
+                <Col align="start">
+                <Card title="Name Node Details">
+                <p><a href={props.Data.namenode}>Name Node WebUI</a></p>
+                </Card>
+                </Col>
+                <Col align="start">
+                <Card title="Data Node Details">
+                    {props.Data.datanodes.map(element =>{
+                        counter++
+                        return <p><a href={element}>Data Node {counter} Manager</a></p>
+                    })}
+                </Card>
+                </Col>
+                <Col align="start">
+                <Card title="Resource Manager Details">
+                <p><a href={props.Data.yarn}>YARN Web UI</a></p>
+                </Card>
+                </Col>
+                <Col align="start">
+                <Card title="Spark Details">
+                <p><a href={props.Data.spark}>Spark UI</a></p>
+                </Card>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default ClusterDashboard;

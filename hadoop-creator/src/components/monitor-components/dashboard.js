ab4c267a41859ea21257b5846a5bb7fe4d21fa7a
John Nooney <1803534@uad.ac.uk>
2022-03-07 16:09:48 +0000

    Add job builder form

--- hadoop-creator/src/components/monitor-components/dashboard.js
+++ hadoop-creator/src/components/monitor-components/dashboard.js
@@ -1,46 +1,133 @@
-import {Row, Col, Select, Divider, Space, Card } from 'antd';
+import {Row, Col, Select, Divider, Space, Card, Form, Input, Button } from 'antd';
 
 
 const { Option } = Select;
 
 function ClusterDashboard(props) {
+    const [jobForm] = Form.useForm();
+
+    function onSubmitClick(){
+
+    }
 
     let counter = 0;
     
     return (
         <div className='cluster_monitor_dashboard'> 
-            <Row justify="center" align="top" gutter={[40,24]}>
-                <Col align="start">
-                    <Card title="Name Node Details">
-                        <p><a href={props.Data.namenode}>Name Node WebUI</a></p>
-                    </Card>
-                </Col>
-                <Col align="start">
-                <Card title="Data Node Details">
-                    {props.Data.datanodes.map(element =>{
-                        counter++
-                        return <p><a href={element}>Data Node {counter} Manager</a></p>
-                    })}
-                </Card>
-                </Col>
-                <Col align="start">
-                    <Card title="Resource Manager Details">
-                        <p><a href={props.Data.yarn}>YARN Web UI</a></p>
-                    </Card>
-                </Col>
-                <Col align="start">
-                    <Card title="Spark Details">
-                        <p><a href={props.Data.spark}>Spark UI</a></p>
+            <Row justify="center" align="top">
+                <Col>
+                    <Card title="Cluster Component Paths">
+                        <Row className justify="center" align="top" gutter={[40,24]}>
+                            <Col align="start">
+                                <Card title="Name Node">
+                                    <p><a href={props.Data.namenode}>Name Node Web UI</a></p>
+                                </Card>
+                            </Col>
+
+                            <Col align="start">
+                            <Card title="Data Node(s)">
+                                {props.Data.datanodes.map(element =>{
+                                    counter++
+                                    return <p><a href={element}>Data Node {counter} Manager</a></p>
+                                })}
+                            </Card>
+                            </Col>
+
+                            <Col align="start">
+                                <Card title="Resource Manager">
+                                    <p><a href={props.Data.yarn}>YARN Web UI</a></p>
+                                </Card>
+                            </Col>
+
+                            <Col align="start">
+                                <Card title="Spark">
+                                    <p><a href={props.Data.spark}>Spark UI</a></p>
+                                </Card>
+                            </Col>
+                        </Row>
                     </Card>
                 </Col>
             </Row>
+            
+
+            <br/>
 
             <Row justify='center' align='middle' gutter={[40, 24]}>
                 <Col align="start">
-                    <Space direction="vertical">
-                        <p>This is where the HDFS API commands will go</p>
-                        <p>Possibly another form here with a drop down box of different jobs to run</p>
-                    </Space>
+                    <Card title="Run Jobs">
+                        <Space direction="vertical">
+                            <p>Select from the dropdown below the type of job you would like to run.</p>
+
+                            <Form
+                            layout="inline"
+                            onFinish={onSubmitClick}
+                            form={jobForm}
+                            >
+
+                                <Form.Item name='job' label="Job Builder">
+                                    <Input.Group compact>
+                                        <Form.Item 
+                                            name={['job','type']} 
+                                            rules={[{required:true, message:'Job type required'}]}
+                                        >
+                                            <Select
+                                                showSearch
+                                                placeholder="Job Type"
+                                                optionFilterProp="children"
+                                                filterOption={(input, option) =>
+                                                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                                }
+                                            >
+                                                <Option value="yarn">Yarn</Option>
+                                                <Option value="spark">Spark</Option>
+                                            </Select>
+                                        </Form.Item>
+
+                                        <Form.Item
+                                            name={['job','operation']} 
+                                            rules={[{required:true, message:'Job operation required'}]}
+                                        >
+                                            <Select
+                                                showSearch
+                                                placeholder="Operation"
+                                                optionFilterProp="children"
+                                                filterOption={(input, option) =>
+                                                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                                }
+                                            >
+                                                <Option value="pi">Pi</Option>
+                                                <Option value="map_reduce">Map Reduce</Option>
+                                            </Select>
+                                            
+                                        </Form.Item>
+
+                                        <Form.Item
+                                            name={['job','modifier']} 
+                                        >
+                                            <Select
+                                                showSearch
+                                                placeholder="Modifier"
+                                                optionFilterProp="children"
+                                                filterOption={(input, option) =>
+                                                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                                }
+                                            >
+                                                <Option value="time_1">Run for 1 minute</Option>
+                                                <Option value="time_2">Run for 2 minutes</Option>
+                                            </Select>
+                                            
+                                        </Form.Item>
+                                    </Input.Group>
+                                </Form.Item>
+
+                                <Form.Item>
+                                    <Button type='primary' htmlType="submit">
+                                        Submit
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </Space>
+                    </Card>
                 </Col>
             </Row>
         </div>

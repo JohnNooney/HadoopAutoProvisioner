f3c6b77a29d386e156d2eaa30cb2d94ce59d541f
John Nooney <1803534@uad.ac.uk>
2022-03-07 11:47:50 +0000

    Add url + port collector

--- flask-api/endpoints/scraper.py
+++ flask-api/endpoints/scraper.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+import ast
 import subprocess
 
 # This endpoint takes care of scraping data from the Hadoop Cluster
@@ -7,28 +8,23 @@ class Scraper(Resource):
         self.origin = 'http://localhost:3000' # only allow request from this host
         self.status = 200
         self.payload = "default"
-        self.namenode = "http://localhost:50070"
-        self.yarn = "http://localhost:8088"
-        self.datanodes = ["http://localhost:8042/", "http://localhost:8043/"] # TODO: set dynamically
-        self.spark = "http://localhost:"
-        self.sparknotebook = "http://localhost:"
+        self.rootUrl = 'http://localhost:'
 
-    def get(self):
+    def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('data', required=True)
 
         args = parser.parse_args()  # parse arguments to dictionary
 
-        print("GET incoming data: ", args['data'])
+        print("scraper POST incoming data: ", args['data'])
+        body = args['data']
+
+        data = self.getClusterUrls(ast.literal_eval(body))
 
         self.status = 200
 
         # TODO: payload should be set with data from getClusterDetails()
-        self.payload = {"payload": {
-            "namenode":self.namenode,
-            "yarn":self.yarn,
-            "datanodes":self.datanodes,
-            "spark":self.spark}}
+        self.payload = {"payload": data}
 
         return self.payload, \
                self.status, \
@@ -36,17 +32,68 @@ class Scraper(Resource):
 
     # method for pre-flight requests. Checks to make sure browser can communicate
     def options(self):
-        return {'Allow': 'GET, OPTIONS'}, \
+        return {'Allow': 'POST, OPTIONS'}, \
                self.status, \
                {'Access-Control-Allow-Origin': self.origin,
                 'Access-Control-Allow-Methods': 'GET, OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type',
                 'Content-Type': 'application/json'}
 
+    def getContainerPort(self, container):
+        print('getting ', container, ' port...')
+        result = subprocess.check_output(['docker', 'port', container])
+        print(container, " request subprocess response: " + result.decode())
+
+        # put result into a format that can be iterated
+        portData = result.decode().replace("\n", "/").split('/')
+
+        # if port data has more than one open port
+        if len(portData) > 3:
+            print(portData)
+            return portData[::2]
+        elif len(portData) == 3:
+            # only one port exists
+            return portData[0]
+        else:
+            return "no port found"
+
+
+
     # if ran in container / stopping will also need to be in container
-    def getClusterDetails(self):
-        # docker - compose - f ./hadoop-cluster/docker-compose.yml up - d
-        # result = subprocess.check_output(['docker', 'compose', '-f', 'hadoop-cluster/base-docker-compose.yml', 'up', '-d'])
-        # print("subprocess response: " + result.decode())
-        # return result.decode()
-        return "test"
+    def getClusterUrls(self, dict):
+
+        nameNode = ''
+        yarnNode = ''
+        dataNodes = []
+        sparkNode = []
+
+        # get the name node's open port
+        nameNode = self.rootUrl + self.getContainerPort('hadoop-cluster-namenode-1')
+        print("namenode port: ", nameNode, "\n")
+
+        # get the yarn node's open port (resource manager)
+        if dict['yarn_resource_manager']:
+            yarnNode = self.rootUrl+self.getContainerPort('hadoop-cluster-resourcemanager-1')
+            print("yarn port: ", yarnNode, "\n")
+
+            # get the node manager's open ports (same as the data node)
+            for i in range(int(dict['yarn_node_managers'])):
+                dataNodePort = self.getContainerPort('hadoop-cluster-nodemanager'+str(i + 1)+'-1')
+                dataNodes.append(self.rootUrl+dataNodePort)
+
+            print("yarn port: ", dataNodes, "\n")
+
+
+        # get the spark container's open port
+        if dict['extras_spark']:
+            sparkNodePorts = self.getContainerPort('hadoop-cluster-spark-1')
+
+            for port in sparkNodePorts:
+                if port:
+                    sparkNode.append(self.rootUrl+port)
+            print("spark ports: ", sparkNode, "\n")
+
+        return {"namenode": nameNode,
+                "yarn": yarnNode,
+                "datanodes": dataNodes,
+                "spark": sparkNode}

e640d1ad5efd5fedc6d9f17b398ced7f07c1ffbc
John Nooney <1803534@uad.ac.uk>
2022-03-14 12:33:16 +0000

    Update w/ static cluster

--- hadoop-creator/src/components/diagram-components/flow-diagram.js
+++ hadoop-creator/src/components/diagram-components/flow-diagram.js
@@ -9,47 +9,92 @@ import ReactFlow, {
 } from 'react-flow-renderer';
 
 function FlowDiagram(props) {
+    //let clusterElements = [];
     const initialElements = [
+        { id: 'namenode', type: 'input', data: { label: 'Name Node' }, position: { x: 150, y: 0 } },
+        { id: 'resourcemanager', data: { label: 'Resource Manager' }, position: { x: 150, y: 100 } },
+        { id: 'nodemanager-1', data: { label: 'Node Manager 1' }, position: { x: 0, y: 200 } },
+        { id: 'nodemanager-2', data: { label: 'Node Manager 2' }, position: { x: 300, y: 200 } },
+        { id: 'datanode-1', data: { label: 'Data Node 1' }, position: { x: 0, y: 300 } },
+        { id: 'datanode-2', data: { label: 'Data Node 2' }, position: { x: 300, y: 300 } },
         {
-          id: 'edges-1',
-          type: 'input',
-          data: { label: 'Input 1' },
-          position: { x: 250, y: 0 },
-        },
-        { id: 'edges-2', data: { label: 'Node 2' }, position: { x: 150, y: 100 } },
-        { id: 'edges-2a', data: { label: 'Node 2a' }, position: { x: 0, y: 180 } },
-        { id: 'edges-3', data: { label: 'Node 3' }, position: { x: 250, y: 200 } },
-        {
-          id: 'edges-e1-2',
-          source: 'edges-1',
-          target: 'edges-2',
-          label: 'bezier edge (default)',
-          className: 'normal-edge',
-        },
-        {
-          id: 'edges-e2-2a',
-          source: 'edges-2',
-          target: 'edges-2a',
+          id: 'edges-nn-rm',
+          source: 'namenode',
+          target: 'resourcemanager',
           type: 'smoothstep',
-          label: 'smoothstep edge',
         },
         {
-          id: 'edges-e2-3',
-          source: 'edges-2',
-          target: 'edges-3',
-          type: 'step',
-          label: 'step edge',
-        }
+          id: 'edges-rm-nm1',
+          source: 'resourcemanager',
+          target: 'nodemanager-1',
+          type: 'smoothstep',
+        },
+        {
+          id: 'edges-rm-nm2',
+          source: 'resourcemanager',
+          target: 'nodemanager-2',
+          type: 'smoothstep',
+        },
+        {
+          id: 'edges-nm1-dn1',
+          source: 'nodemanager-1',
+          target: 'datanode-1',
+          type: 'smoothstep',
+        },
+        {
+          id: 'edges-nm2-dn2',
+          source: 'nodemanager-2',
+          target: 'datanode-2',
+          type: 'smoothstep',
+        },
     ];
-
-    const onLoad = (reactFlowInstance) => reactFlowInstance.fitView();
-    const onNodeDragStop = (event, node) => console.log('drag stop', node);
-    const onElementClick = (event, element) => console.log('click', element);
-    
     const [elements, setElements] = useState(initialElements);
-    
-    const onElementsRemove = (elementsToRemove) => setElements((els) => removeElements(elementsToRemove, els));
-    const onConnect = (params) => setElements((els) => addEdge(params, els));
+
+    function buildDiagram(list){
+
+        //build namenode
+        var namenode = {};
+        list.push(namenode);
+
+        //build resource manager
+        var resourcemanager = {};
+        list.push(resourcemanager);
+
+        //build node managers
+        var nodemanager = {};
+        list.push(nodemanager);
+
+        //build data nodes
+        var datanode = {};
+        list.push(datanode);
+
+        //build spark
+        var spark = {};
+        list.push(spark);
+
+        return list;
+    }
+
+    function onLoad(reactFlowInstance){
+        //set buildDiagram()
+        reactFlowInstance.fitView();
+    }
+
+    function onNodeDragStop(event, node){
+        console.log('drag stop', node);
+    }
+
+    function onElementClick(event, element){
+        console.log('click', element);
+    }
+
+    function onElementsRemove(elementsToRemove){
+        setElements((els) => removeElements(elementsToRemove, els));
+    }
+
+    function onConnect(params){
+        setElements((els) => addEdge(params, els));
+    }
     
     return (
     <div className="flow">
